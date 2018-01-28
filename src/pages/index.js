c17359c86c8c6b184a879f941281771b67c0dca0
Spencer Sanchez <dev@spencersanchez.com>
2018-01-27 20:25:49 -0500

    List generated pages on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { object } from 'prop-types'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends Component {
+	render() {
+		const { edges: people } = this.props.data.allRandomUser
+		return (
+			<main>
+				{people.map(({ node: user }) => (
+					<div key={user.id}>
+						<Link to={`/${user.nat}/${user.name.first}_${user.name.last}`}>
+							{user.name.first} {user.name.last}
+						</Link>
+					</div>
+				))}
+			</main>
+		)
+	}
+}
+
+IndexPage.propTypes = {
+	data: object,
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+	query PageQuery {
+		allRandomUser {
+			edges {
+				node {
+					id
+					name {
+						first
+						last
+					}
+					nat
+				}
+			}
+		}
+	}
+`
